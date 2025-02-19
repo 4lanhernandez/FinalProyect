4a7b54f4c6e8497ffb52942d2d41ea6357f34f23
GLJROB3 <cshtd.computacion@gmail.com>
2025-02-18 22:15:49 -0600

    agregué funciones :hammer:

--- FInalProyect.py
+++ FInalProyect.py
@@ -11,11 +11,42 @@ def mostrar_menu():
     print("5️⃣ Salir 🚪")
     print("=" * 40)
 
-    def agregar_contacto():
+#Escribirá el nombre del contacto,luego su numero e imprimirá "guardado", pero si este ya ha sido guardado; imprimirá "ya existe"
+def agregar_contacto():
     nombre = input("📝 Ingresa el nombre del contacto: ").strip().capitalize()
     if nombre in agenda:
-        print(f"⚠️ El contacto '{nombre}' ya existe.")
+         print(f"⚠️ El contacto '{nombre}' ya existe.")
     else:
         telefono = input("📞 Ingresa el número de teléfono: ").strip()
         agenda[nombre] = telefono
         print(f"✅ Contacto '{nombre}' agregado exitosamente.")
+
+#1-Buscará el nombre del contacto, pero si este contacto NO existe, "imprimirá  no existe".
+def buscar_contacto():
+    nombre = input("🔍 Ingresa el nombre a buscar: ").strip().capitalize()
+    if nombre in agenda:
+        print(f"☎️ {nombre}: {agenda[nombre]}")
+    else:
+        print(f"❌ El contacto '{nombre}' no existe.")
+
+#2-Escribirá el nombre del contacto y SI existe, imprimirá "eliminado". Si NO existe imprimirá "no existe"
+def eliminar_contacto():
+    nombre = input("❌ Ingresa el nombre a eliminar: ").strip().capitalize()
+    if nombre in agenda:
+        del agenda[nombre]
+        print(f"✅ Contacto '{nombre}' eliminado.")
+    else:
+        print(f"⚠️ El contacto '{nombre}' no existe.")
+
+#3-Si no hay contactos GUARDADOS, imprimirá "La agenda está vacía". Si hay contactos GUARDADOS, imprimirá "Lista de contactos" y serán mostrados los contactos guardados anteriormente)
+def mostrar_contactos():
+    if not agenda:
+        print("📂 La agenda está vacía.")
+    else:
+        print("\n📋 LISTA DE CONTACTOS 📋")
+        print("-" * 40)
+        for nombre, telefono in agenda.items():
+            print(f"📌 {nombre}: {telefono}")
+        print("-" * 40)
+
+        
